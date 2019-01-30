b0340639cb5ad70b135cd553e7497e651ca71ebd
sabelogumede <iamsabelogumede@gmail.com>
2019-01-30 11:59:29 +0200

    initialized our nuxt app and added a post, admin,about directory. also added post pade and id-for an individual post. added a font in layouts default page.

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about-page">
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus pretium erat, non tempus lorem dignissim sit amet. </h1>
+        <p>Fusce eget felis nunc. Mauris ligula dolor, vestibulum ac dui sit amet, pulvinar mollis risus.</p>
+        <p>Sed semper, magna et dictum convallis, urna augue tincidunt sem, sed pulvinar massa velit a eros. Pellentesque consequat quam eu mollis bibendum.</p>
+        <p>Curabitur ultricies pretium nunc nec elementum. Ut nec nulla orci. Vestibulum vehicula enim mi, id aliquet elit finibus egestas.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at nisi ut ligula fermentum interdum. Donec volutpat mi ut risus pulvinar sodales.</p>
+    </div>
+</template>
+
+<style scoped>
+    .about-page {
+        text-align: center;
+        padding: 30px;
+    }
+</style>
+
---
+++ pages/post/_id/index.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="single-post-page">
+        <section class="post">
+            <h1 class="post-title">Title of the Post</h1>
+            <div class="post-details">
+                <div class="post-detail">Last updated on XXX</div>
+                <div class="post-detail">Written by NAME</div>
+            </div>
+                <p class="post-content">Content of the post: Donec aliquam mollis diam, at pulvinar elit tempus vel. Aenean blandit purus orci, in molestie urna bibendum non. Morbi finibus, ipsum a imperdiet consequat, felis nisi placerat augue, at aliquet metus nunc at dolor. Morbi tincidunt nec velit id efficitur. In mattis malesuada magna, at viverra risus auctor ac. Nam a diam scelerisque, porttitor odio vitae, semper ex. </p>
+        </section>
+        <section class="post-feedback">
+            <p>Let me know what you think about the post, send a mail to <a href="mailto:feedback@my+awesome-domain.com">feedback@my+awesome-domain.com</a>.</p>
+        </section>
+
+    </div>
+</template>
+
+<style scoped>
+    .single-post-page {
+        padding: 30px;
+        text-align: center;
+        box-sizing: border-box;
+    }
+
+    .post {
+        width: 100%;
+    }
+
+    @media (min-width: 768px) {
+        .post {
+            width: 100%;
+        }
+    }
+
+    @media (min-width: 768px) {
+        .post {
+            width: 600px;
+            margin: auto;
+        }
+    }
+
+    .post-title {
+        margin: 0;
+    }
+
+    .post-details {
+        padding: 10px;
+        box-sizing: border-box;
+        border-bottom: 3px solid #ccc;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    
+    /*  */
+    @media (min-width: 768px) {
+        .post-details {
+        flex-direction: row;
+        }
+    }
+
+
+    .post-detail {
+        color: rgb(88, 88, 88);
+        margin: 0 10px;
+    }
+
+    .post-feedback a {
+        color: red;
+        text-decoration: none;
+    }
+
+    .post-feedback a:hover,
+    .post-feedback a:active {
+        color: salmon;
+    }
+
+</style>
