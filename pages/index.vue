a563efbd6928c10c2f1339a25286de367a3deca7
sabelogumede <iamsabelogumede@gmail.com>
2019-01-30 15:48:37 +0200

    splited the page into componets

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,46 @@
 <template>
   <div class="home-page">
     <section class="intro">
-      <h1>Get the latest tech news!</h1>
+      <h1>Busy Green Living News!</h1>
     </section>
     <section class="featured-posts">
-      <!-- first post -->
-      <nuxt-link :to="'/posts/' + 1" class="post-preview">
-        <article>
-          <div class="post-thumbnail" style="background-image: url('https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj0vKeBp5XgAhWFsnEKHdeGDzwQjRx6BAgBEAU&url=http%3A%2F%2Fwww.edsgreenenergy.com%2F&psig=AOvVaw19YsCUXLsxukZIcS0P90Re&ust=1548930777758014')"></div>
-          <div class="post-content">
-            <h1>Post Title 1</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <!-- second post -->
-      <nuxt-link :to="'/posts/' + 2" class="post-preview">
-        <article>
-          <div class="post-thumbnail"></div>
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-            <p>Preview Text</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <PostPreview 
+        id="1"
+        thumbnail= "http://www.organicexpo.co.nz/wp-content/uploads/2011/12/glm-march-2017-small.jpg"
+        title="Hello there!"
+        previewText="This is my first post" />
+      <PostPreview 
+        id="2"
+        thumbnail= "http://www.organicexpo.co.nz/wp-content/uploads/2011/12/glm-march-2017-small.jpg"
+        title="Hello there!"
+        previewText="This is my first post" />
+      <PostPreview 
+        id="3"
+        thumbnail= "http://www.organicexpo.co.nz/wp-content/uploads/2011/12/glm-march-2017-small.jpg"
+        title="Hello there!"
+        previewText="This is my first post" />
+      <PostPreview 
+        id="4"
+        thumbnail= "http://www.organicexpo.co.nz/wp-content/uploads/2011/12/glm-march-2017-small.jpg"
+        title="Hello there!"
+        previewText="This is my first post" />
+      
     </section>
   </div>
 </template>
 
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+    components: {
+        PostPreview
+    }
+}
+</script>
 
 <style scoped>
+
 .intro {
   height: 300px;
   position: relative;
@@ -56,7 +67,6 @@
   }
 }
 
-
 .featured-posts {
   display: flex;
   padding: 20px;
@@ -64,41 +74,6 @@
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 
 </style>
