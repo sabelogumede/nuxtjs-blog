8334047598925c01608e20283ba4f221743b70cc
sabelogumede <iamsabelogumede@gmail.com>
2019-01-31 11:39:02 +0200

    initialized admin pages

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,13 +1,36 @@
 <template>
     <div class="admin-page">
-        <h2>admin page</h2>
+        <section class="new-post">
+            <button>Create Post</button>
+        </section>
+        <section class="existing-posts">
+            <h1>Existing Posts</h1>
+            <PostList />
+        </section>
     </div>
 </template>
 
-<style>
+<script>
+import PostList from '@/components/Posts/PostList'
+
+export default {
+    components: {
+        PostList
+    } 
+}
+</script>
+
+
+<style scoped>
     .admin-page {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        padding: 20px;
+    }
+    .new-post {
+        text-align: center;
+        border-bottom: 2px solid #ccc;
+        padding-bottom: 10px;
+    }
+    .existing-posts {
+        text-align: center;
     }
 </style>
