1ffa578bdd69a5eb8ed0326c422e9c599fda5ba8
sabelogumede <iamsabelogumede@gmail.com>
2019-02-01 11:57:47 +0200

    added a new layout for admin without the header and addedd our custom error page

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -10,9 +10,22 @@
 import AdminPostForm from '@/components/Admin/AdminPostForm'
 
 export default {
+    layout: 'admin',
     components: {
         AdminPostForm
     }
 }
 </script>
 
+<style scoped>
+    .new-post-form {
+        width: 90%;
+        margin: 20px auto;
+    }
+
+    @media (min-width: 768px) {
+        .new-post-form {
+            width: 500px;
+        }
+    }
+</style>
